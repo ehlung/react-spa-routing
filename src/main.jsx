00dd4b13b59c9dbfb7fb63746ecb20c8f1c21a2b
Yeryeong Kang <dpfud8786@gmail.com>
2026-01-09 22:09:45 +0900

    feat: 라우터 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./app/App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
 import { ThemeProvider } from "./app/providers/ThemeProvider.jsx";
+import { SearchProvider } from "./app/providers/SearchProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <SearchProvider>
+        <RouterProvider router={router} />
+      </SearchProvider>
     </ThemeProvider>
   </StrictMode>
 );
